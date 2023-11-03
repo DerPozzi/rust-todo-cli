f66d9cd21c0cfe8359ff11a0cab50e31e6683974
Gianluca Pozzo <gianluca.pozzo@sva.de>
2023-11-03 14:26:03 +0100

    Split db logic into own module

---
+++ src/database.rs
@@ -0,0 +1,66 @@
+// TODO: select todo by id
+use sqlx::postgres::PgPoolOptions;
+
+use sqlx::{FromRow, Pool, Postgres};
+
+#[derive(Debug, FromRow)]
+pub struct DbTodo {
+    pub id: i32,
+    pub title: String,
+    pub description: String,
+    pub completed: bool,
+}
+pub struct Todo {
+    title: String,
+    description: String,
+    completed: bool,
+}
+impl Todo {
+    pub fn new(title: String, desc: String, completed: bool) -> Self {
+        Todo {
+            title: title,
+            description: desc,
+            completed: completed,
+        }
+    }
+}
+pub async fn create_db_pool(url: &str) -> Result<Pool<Postgres>, sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&url)
+        .await?;
+    Ok(pool)
+}
+
+pub async fn create_table(pool: &Pool<Postgres>) -> Result<(), sqlx::Error> {
+    sqlx::query(
+        r#"
+CREATE TABLE IF NOT EXISTS todo (
+    id SERIAL PRIMARY KEY,
+    title text NOT NULL,
+    description text,
+    completed bool
+);        "#,
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
+
+pub async fn get_all_tasks(pool: &Pool<Postgres>) -> Result<Vec<DbTodo>, sqlx::Error> {
+    let rows = sqlx::query_as::<_, DbTodo>("SELECT * FROM todo")
+        .fetch_all(pool)
+        .await?;
+    Ok(rows)
+}
+
+pub async fn insert_new_todo(pool: &Pool<Postgres>, task: Todo) -> Result<(), sqlx::Error> {
+    sqlx::query("INSERT INTO todo (title, description, completed) VALUES ($1, $2, $3) ")
+        .bind(task.title)
+        .bind(task.description)
+        .bind(task.completed)
+        .execute(pool)
+        .await?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,33 @@
-use dotenv;
-use sqlx::postgres::{PgPoolOptions, PgRow};
-use sqlx::{FromRow, Row};
+// TODO: Clap Argument parsing
+// TODO: Pretty print the tasks
 
-#[derive(Debug, FromRow)]
-struct Todo {
-    id: i64,
-    title: String,
-    descripion: String,
-    completed: bool,
-}
+use dotenv;
+
+mod database;
+use database::{create_db_pool, create_table, get_all_tasks, insert_new_todo, Todo};
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     dotenv::dotenv().ok();
 
+    let new_task = Todo::new("Title".to_string(), "Description".to_string(), false);
     let db_url = std::env::var("DB_URL").expect("db_url must be set");
 
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&db_url)
-        .await?;
+    let pool = create_db_pool(&db_url).await?;
     // Create table if not exist
-    sqlx::query(
-        r#"
-CREATE TABLE IF NOT EXISTS todo (
-    id SERIAL PRIMARY KEY,
-    title text NOT NULL,
-    description text,
-    completed bool
-);        "#,
-    )
-    .execute(&pool)
-    .await?;
+    create_table(&pool).await?;
+    match insert_new_todo(&pool, new_task).await {
+        Ok(_) => {}
+        Err(msg) => println!("ERROR: {}", msg),
+    }
+    let rows = get_all_tasks(&pool).await?;
 
-    let rows = sqlx::query("SELECT * FROM todo").fetch_all(&pool).await?;
-
-    let str_result = rows
-        .iter()
-        .map(|r| {
-            format!(
-                "{} - {} : {}",
-                r.get::<String, _>("title"),
-                r.get::<String, _>("description"),
-                r.get::<bool, _>("completed")
-            )
-        })
-        .collect::<Vec<String>>()
-        .join("\n");
-    println!("{}", str_result);
-
-    sqlx::query("INSERT INTO todo (title, description, completed) VALUES ($1, $2, $3)")
-        .bind("test")
-        .bind("Beschreibung")
-        .bind(false)
-        .execute(&pool)
-        .await?;
-    let rows = sqlx::query("SELECT * FROM todo").fetch_all(&pool).await?;
-
-    let str_result = rows
-        .iter()
-        .map(|r| {
-            format!(
-                "{} - {} : {}",
-                r.get::<String, _>("title"),
-                r.get::<String, _>("description"),
-                r.get::<bool, _>("completed")
-            )
-        })
-        .collect::<Vec<String>>()
-        .join("\n");
-    println!("{}", str_result);
-
-    sqlx::query("DROP TABLE todo").execute(&pool).await?;
+    for task in rows {
+        println!(
+            "Id: {} - {} - {} : {}",
+            task.id, task.title, task.description, task.completed
+        )
+    }
 
     Ok(())
 }
